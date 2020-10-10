6280e8dd131add8e6f0d4cbaad6160efe9687558
Alexander Bird <alex@alexanderbird.software>
2020-10-09 19:25:58 -0700

    Switch to multi-column layout; better errors for thumbnail fetch; switch tags to categories

--- download_thumbnail.sh
+++ download_thumbnail.sh
@@ -23,10 +23,13 @@ file_path="./build/$isbn"
 
 api="https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn&key=$GOOGLE_BOOKS_API_KEY"
 
-thumbnail_url=$(curl "$api" | jq -r ".items[0].volumeInfo.imageLinks.smallThumbnail")
+search_results=$(curl "$api")
+
+thumbnail_url=$(echo "$search_results" | jq -r ".items[0].volumeInfo.imageLinks.smallThumbnail")
 
 [[ "$thumbnail_url" == "" ]] || [[ "$thumbnail_url" == "null" ]] && {
-  echo "Problem retrieving the thumbnail url for $isbn"
+  echo "Problem retrieving the thumbnail url for $isbn. Found JSON: "
+  echo "$search_results" | jq
   exit 1
 }
 
