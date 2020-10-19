d342b3c98bb045f2f78d1002e34c9c7a0c4be32c
Alexander Bird <alex@alexanderbird.software>
2020-10-19 02:46:11 -0700

    Add rough support for NOT and OR search.
    The search is wonky. Probably we'd need actual unit tests to get it
    working -- so far I've just been tinkering and that's going about as
    well as you would think it would.
    
    Still, the basic AND/OR/NOT works.
    
    It's complicated nesting that gets weird.

--- src/javascript/search_language.js
+++ src/javascript/search_language.js
@@ -14,6 +14,32 @@ const searchLanguage = (function() {
     matches(book) {
       return this.expressions.every(expression => expression.matches(book));
     }
+
+    toString() { return this.expressions.length > 1 ? `AND(${this.expressions.join(',')})` : this.expressions.length[0]; }
+  }
+
+  class OrExpressionList /* implements Expression */ {
+    constructor(...expressions) {
+      this.expressions = expressions;
+    }
+
+    matches(book) {
+      return this.expressions.some(expression => expression.matches(book));
+    }
+
+    toString() { return this.expressions.length > 1 ? `OR(${this.expressions.join(',')})` : this.expressions.length[0]; }
+  }
+
+  class NotExpressionList /* implements Expression */ {
+    constructor(expression) {
+      this.expression = expression;
+    }
+
+    matches(book) {
+      return !this.expression.matches(book);
+    }
+
+    toString() { return `NOT(${this.expression})`; }
   }
 
   class ScopedSearchExpression /* implements Expression */ {
@@ -26,6 +52,8 @@ const searchLanguage = (function() {
       const value = book.getDataSetEntry(this.scope);
       return value.toLowerCase().indexOf(this.search) >= 0;
     }
+
+    toString() { return `${this.scope}:${this.search}`; }
   }
 
   class RegularSearchExpression /* implements Expression */ {
@@ -36,6 +64,8 @@ const searchLanguage = (function() {
     matches(book) {
       return book.lowerCaseText.indexOf(this.search) >= 0;
     }
+
+    toString() { return this.search; }
   }
 
   class YesExpression /* implements Expression */ {
@@ -49,32 +79,61 @@ const searchLanguage = (function() {
       this.pattern = pattern;
       this.expressionFactory = expressionFactory;
     }
+
+    appliesTo(text) {
+      return text.match(this.pattern);
+    }
   }
 
-  const listExpressionRules = [
-    new Rule(/^$/, () => new YesExpression()),
-    new Rule(/^.*$/, list => new AndExpressionList(...list.toLowerCase().split(' ')
-      .map(expression => parseSingleSearchExpression(expression))))
-  ];
+  function parseList(list, expressionFactory) {
+    const rules = listItemRules;
+    let rawList = list.trim();
+    const expressions = [];
+    while(rawList) {
+      rules.some(rule => {
+        const match = rule.appliesTo(rawList);
+        if (match) {
+          rawList = rawList.replace(match[0], '').trim();
+          expressions.push(rule.expressionFactory(...match));
+          return true;
+        }
+        return false;
+      });
+    }
 
-  function parseSearchExpressionList(expressionString = '') {
+    return expressionFactory(...expressions);
+  }
+
+  function parseListParent(expressionString) {
+    const rules = listParentRules;
     const text = expressionString.trim();
-    const rule = listExpressionRules.find(r => text.match(r.pattern));
-    return rule.expressionFactory(text);
+    for (const rule of rules) {
+      const match = rule.appliesTo(text);
+      if (match) { 
+        return rule.expressionFactory(...match);
+      }
+    }
   }
 
-  const singleExpressionRules = [
-    new Rule(/[^:]+:[^:]+( |$)/, text => new ScopedSearchExpression(...text.split(':'))),
-    new Rule(/[^ ]+( |$)/, text => new RegularSearchExpression(text))
+  const listParentRules = [
+    new Rule(/^$/,                    () => new YesExpression()),
+    new Rule(/^OR\((.*)\)$/,   (_, list) => parseList(list, (...x) => new OrExpressionList(...x))),
+    new Rule(/^AND\((.*)\)$/i, (_, list) => parseList(list, (...x) => new AndExpressionList(...x))),
+    new Rule(/^NOT\((.*)\)$/i, (_, list) => new NotExpressionList(parseListParent(list))),
+    new Rule(/^\((.*)\)$/,     (_, list) => parseList(list, (...x) => new AndExpressionList(...x))),
+    new Rule(/^.*$/,                list => parseList(list, (...x) => new AndExpressionList(...x)))
   ];
 
-  function parseSingleSearchExpression(text) {
-    const rule = singleExpressionRules.find(r => text.match(r.pattern));
-    return rule.expressionFactory(text);
-  }
+  const listItemRules = [
+    new Rule(/OR\((.*)\)( |$)/i,   (_, list) => parseList(list, (...x) => new OrExpressionList(...x))),
+    new Rule(/AND\((.*)\)( |$)/i,  (_, list) => parseList(list, (...x) => new AndExpressionList(...x))),
+    new Rule(/NOT\((.*)\)( |$)/i,  (_, list) => new NotExpressionList(parseListParent(list))),
+    new Rule(/([^:]+:[^:]+)( |$)/, (_, text) => new ScopedSearchExpression(...text.split(':'))),
+    new Rule(/([^ ]+)( |$)/,       (_, text) => new RegularSearchExpression(text))
+  ];
 
   return {
-    parse: parseSearchExpressionList
+    parse: parseListParent
   }
 }());
 
