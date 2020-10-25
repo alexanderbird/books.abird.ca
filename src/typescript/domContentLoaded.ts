53475a77a194af3818cfe8251ec799fe16aff11a
Alexander Bird <alex@alexanderbird.software>
2020-10-25 15:34:26 -0700

    Remove 'hello, TypesScript' console log

--- src/typescript/domContentLoaded.ts
+++ src/typescript/domContentLoaded.ts
@@ -2,8 +2,6 @@ import * as filter from './filter';
 import * as search from './search';
 
 export function domContentLoaded(): void {
-  console.log('Hello, TypeScript (and Babel and Webpack 🤦)');
-
   filter.updateFromUrlHash();
   filter.initialize();
   search.initialize();
