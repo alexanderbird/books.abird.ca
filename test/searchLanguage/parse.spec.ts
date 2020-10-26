192918f7fecdbd2bdce0893f0b7b5f07223e122d
Alexander Bird <alex@alexanderbird.software>
2020-10-25 19:36:11 -0700

    Add support for AND OR and scoped search terms

--- test/searchLanguage/parse.spec.ts
+++ test/searchLanguage/parse.spec.ts
@@ -2,11 +2,29 @@ import { parse } from '../../src/typescript/searchLanguage/parse';
 
 describe('searchLanguage parser', () => {
   it('parses one word search terms', () => {
-    expect(parse(`hello`)).toEqual({ type: 'search', value: 'hello' });
+    expect(parse('hello')).toEqual({ type: 'search', value: 'hello' });
+  });
+
+  it('parses scoped search terms', () => {
+    expect(parse('foo:bar')).toEqual({ type: 'scoped', value: { scope: 'foo', search: 'bar' } });
   });
 
   it('parses multi-word search terms as an and expression', () => {
-    expect(parse(`hello world`)).toEqual({ type: 'and', value: [
+    expect(parse('hello world')).toEqual({ type: 'and', value: [
+      { type: 'search', value: 'hello' },
+      { type: 'search', value: 'world' },
+    ]});
+  });
+
+  it('parses AND expressions', () => {
+    expect(parse('hello AND world')).toEqual({ type: 'and', value: [
+      { type: 'search', value: 'hello' },
+      { type: 'search', value: 'world' },
+    ]});
+  });
+
+  it('parses OR expressions', () => {
+    expect(parse('hello OR world')).toEqual({ type: 'or', value: [
       { type: 'search', value: 'hello' },
       { type: 'search', value: 'world' },
     ]});
